1f6570eebd81e870bfb37d2073ed1538327d6eaa
Monique Rio <mrio@umich.edu>
2021-02-03 09:34:07 -0500

    added pots, put, and get_all functionality

--- lib/alma_rest_client.rb
+++ lib/alma_rest_client.rb
@@ -4,6 +4,14 @@ require 'httparty'
 module AlmaRestClient
   class Error < StandardError; end
   # Your code goes here...
+  class Response
+    attr_reader :code, :message, :parsed_response
+    def initialize(code: 200, message: 'Success', parsed_response: {})
+      @code = code
+      @message = message
+      @parsed_response = parsed_response
+    end
+  end
   class Client
     include HTTParty
     base_uri "#{ENV.fetch('ALMA_API_HOST')}/almaws/v1"
@@ -16,36 +24,55 @@ module AlmaRestClient
     def get(url, query={})
       self.class.get(url, query: query)
     end
-    #def post(url, query={})
-      #self.class.post(url, query: query)
-    #end
+    def post(url, query={})
+      self.class.post(url, query: query)
+    end
 
-    #def put(url, body)
-      #self.class.headers 'Content-Type' => 'application/json'
-      #self.class.put(url, { body: body.to_json } )
-    #end
+    #requires valid json for the body
+    def put(url, body)
+      self.class.headers 'Content-Type' => 'application/json'
+      self.class.put(url, { body: body } )
+    end
 
-    #def get_all(url:, record_key:, limit: 100, query: {})
-      #query[:offset] = 0 
-      #query[:limit] = limit
-      #output = get(url, query)
-      #if output.code == 200
-        #body = output.parsed_response
-        #while  body['total_record_count'] > limit + query[:offset]
-          #query[:offset] = query[:offset] + limit
-          #my_output = get(url, query) 
-          #if my_output.code == 200
-            #my_output.parsed_response[record_key].each {|x| body[record_key].push(x)}
-          #else
-            #return my_output #return error
-          #end
-        #end 
-        #::Response.new(parsed_response: body) #return good response
-      #else
-        #output #return error
-      #end
-    #end 
+    #record_key is the key that holds the array of items 
+    def get_all(url, record_key, limit=100, query={})
+      try_count = 1
+      while try_count <= 2
+        response = get_all_loop(url, record_key, limit, query)
+        if response.code == 200
+          return response
+        else
+          try_count = try_count + 1
+        end
+      end
+      Response.new(code: 500, message: 'Could not retrieve items.')
+    end 
+
+    private
+    #query keys 'limit' and 'offset' will be overwritten
+    def get_all_loop(url, record_key, limit, query={})
+      query[:offset] = 0 
+      query[:limit] = limit
+      output = get(url, query)
+      if output.code == 200
+        body = output.parsed_response
+        while  body['total_record_count'] > limit + query[:offset]
+          query[:offset] = query[:offset] + limit
+          my_output = get(url, query) 
+          if my_output.code == 200
+            my_output.parsed_response[record_key].each {|x| body[record_key].push(x)}
+          else
+            return Response.new(code: 500, message: 'Could not retrieve items.')
+          end
+        end 
+        Response.new(parsed_response: body) #return good response
+      else
+        return Response.new(code: 500, message: 'Could not retrieve items.')
+      end
+    end
   end
+
+
   def self.client
     Client.new
   end
--- spec/alma_rest_client_spec.rb
+++ spec/alma_rest_client_spec.rb
@@ -24,5 +24,60 @@ RSpec.describe AlmaRestClient::Client do
       expect(subject.get('/users/soandso/loans', {"limit" => 100}).class.name).to eq("HTTParty::Response")
     end
   end
+  context "#post(url, query={})" do
+    it "returns HTTParty response for only url" do
+      stub_alma_post_request(url: 'users/soandso/loans')
+      expect(subject.post('/users/soandso/loans').class.name).to eq("HTTParty::Response")
+    end
+    it "returns HTTParty response for url and parameters" do
+      stub_alma_post_request(url: 'users/soandso/loans', query: {"op" => "renew"})
+      expect(subject.post('/users/soandso/loans', {"op" => "renew"}).class.name).to eq("HTTParty::Response")
+    end
+  end
+  context "#put(url, body)" do
+    it "returns HTTParty response for url and body" do
+      input = 'iamastring'.to_json
+      stub_alma_put_request(url: 'users/soandso', input: input, output: "{}" )
+      expect(subject.put('/users/soandso', input).class.name).to eq("HTTParty::Response")
+    end
+  end
+  context "#get_all(url, record_key, limit, query)" do
+    it "makes multiple calls to the alma server and returns appropriate response." do
+      url = "users/jbister/loans"
+      stub_alma_get_request( query: { "limit" => 1, "offset" => 0}, body: File.read("./spec/fixtures/jbister_loans0.json"), url: url) 
+      stub_alma_get_request( query: { "limit" => 1, "offset" => 1}, body: File.read("./spec/fixtures/jbister_loans1.json"), url: url) 
+
+      response = described_class.new.get_all("/#{url}", "item_loan", 1)
+      expect(response.class.name).to eq("AlmaRestClient::Response")
+      expect(response.code).to eq(200)
+      expect(response.parsed_response["item_loan"].count).to eq(2)
+    end
+    it "if alma requests fail to get everything, it tries again and if it fails again it returns an error" do
+      url = "users/jbister/loans"
+      stub1 = stub_alma_get_request( query: { "limit" => 1, "offset" => 0}, body: File.read("./spec/fixtures/jbister_loans0.json"), url: url) 
+      stub2 = stub_alma_get_request( query: { "limit" => 1, "offset" => 1}, body: "", url: url, status: 500) 
+      response = described_class.new.get_all("/#{url}", "item_loan", 1)
+      expect(response.class.name).to eq("AlmaRestClient::Response")
+      expect(response.code).to eq(500)
+      expect(response.message).to eq('Could not retrieve items.')
+      expect(stub1).to have_been_requested.times(2)
+      expect(stub2).to have_been_requested.times(2)
+    end
+    it "if alma requests fail to get everything the first time, it tries again and if it succeeds returns successful full results" do
+      url = "users/jbister/loans"
+      stub1 = stub_alma_get_request( query: { "limit" => 1, "offset" => 0}, body: File.read("./spec/fixtures/jbister_loans0.json"), url: url) 
+      stub2 = stub_alma_get_request( query: { "limit" => 1, "offset" => 1}, body: "", url: url, status: 500) 
+      stub2.then.to_return({body: File.read("./spec/fixtures/jbister_loans1.json"), status: 200, headers: {content_type: 'application/json'}}) 
+
+      response = described_class.new.get_all("/#{url}", "item_loan", 1)
+      expect(stub1).to have_been_requested.times(2)
+      expect(stub2).to have_been_requested.times(2)
+
+      expect(response.class.name).to eq("AlmaRestClient::Response")
+      expect(response.code).to eq(200)
+      expect(response.parsed_response["item_loan"].count).to eq(2)
+    end
+
+  end
   
 end
