c426b654b8a7a9a3020334a27950327929b478af
Monique Rio <mrio@umich.edu>
2021-12-08 13:06:25 -0500

    refactored report method

--- lib/alma_rest_client.rb
+++ lib/alma_rest_client.rb
@@ -13,7 +13,7 @@ module AlmaRestClient
       @parsed_response = parsed_response
     end
   end
-  class Client
+  class ClientBase
     include HTTParty
     base_uri "#{ENV.fetch('ALMA_API_HOST')}/almaws/v1"
 
@@ -27,6 +27,13 @@ module AlmaRestClient
       define_method(name) do |url, options={}|
         self.class.public_send(name, url, options)
       end
+    end
+  end
+  class Client 
+    extend Forwardable
+    def_delegators :@base_client, :get, :post, :delete, :put
+    def initialize(base_client=ClientBase.new)
+      @base_client = base_client
     end
 
     #record_key is the key that holds the array of items 
@@ -46,53 +53,54 @@ module AlmaRestClient
     def get_report(path:, filter: nil)
       query = { path: path, limit: 1000, col_names: true}
       try_count = 1
-      while try_count <= 2
-        response = report_loop(**query)
-        if response.code == 200
-          return response
+      begin 
+        report_loop(**query)
+      rescue 
+        if try_count < 2
+          try_count = try_count+1
+          retry
         else
-          try_count = try_count + 1
+          return Response.new(code: 500, message: 'Could not retrieve report.')
         end
       end
-      Response.new(code: 500, message: 'Could not retrieve report.')
     end
 
     private
+    def get_columns(xml)
+      col_raw = xml["QueryResult"]["ResultXml"]["rowset"]["schema"]["complexType"]["sequence"]["element"]
+      cols = col_raw.map {|x| [x["name"],  x["saw_sql:columnHeading"]] }.to_h
+    end
 
     def report_loop(path:,limit:,col_names:)
       output = []
-      columns = {}
       query = {path: path, limit: limit, col_names: col_names }
-      response = get("/analytics/reports", query: query)
-      if response.code != 200 
-        return Response.new(code: 500, message: 'Could not retrieve report.')
+      start_report(query) do |row|
+        output.push(row)
       end
+      Response.new(parsed_response: output)
+    end
+    def start_report(query, &block)
+      response = get("/analytics/reports", query: query)
+
       xml_string = response.parsed_response["anies"].first
       xml = Hash.from_xml(xml_string)
       query[:token] = xml["QueryResult"]["ResumptionToken"]
-      col_raw = xml["QueryResult"]["ResultXml"]["rowset"]["schema"]["complexType"]["sequence"]["element"]
-      col_raw.each {|x| columns[x["name"]] = x["saw-sql:columnHeading"] }
-
-      loop do
+      columns = get_columns(xml)
+      report_actual_loop(xml, columns, query, &block) 
+    end
+    def report_actual_loop(xml, columns, query, &block)
         rows = xml["QueryResult"]["ResultXml"]["rowset"]["Row"]
         rows = [ rows ] if rows.class == Hash
         rows.each do |row|
           my_row = {}
           row.keys.each {|k| my_row[columns[k]] = row[k] }
-          output.push(my_row)
+          block.call(my_row)
         end
-        if xml["QueryResult"]["IsFinished"] == 'true'
-          break
-        else
+        if xml["QueryResult"]["IsFinished"] != 'true'
           response = get("/analytics/reports", query: query)
-          if response.code == 200          
-            xml = Hash.from_xml(response.parsed_response["anies"].first)
-          else
-            return Response.new(code: 500, message: 'Could not retrieve report.')
-          end
+          xml = Hash.from_xml(response.parsed_response["anies"].first)
+          report_actual_loop(xml, columns, query, &block)
         end
-      end
-      Response.new(parsed_response: output)
     end
     #query keys 'limit' and 'offset' will be overwritten
     def get_all_loop(url, record_key, limit, query={})
